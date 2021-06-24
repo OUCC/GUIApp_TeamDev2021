e5f8ef0bc0c85c6854321e54d24f542430ccdc87
watamario15 <watamario15@outlook.jp>
2021-06-24 22:48:18 +0900

    Implement necessary features.

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 #include "db.hpp"
 
 using App = SceneManager<String>;
+db Database;
+Array<single_data> passArray; //パスワードの三項目をセットにした配列
 
 // ログインシーン
 class Login : public App::Scene {
@@ -12,7 +14,7 @@ private:
     ColorF *buttonColor;
     Circle *button;
     Transition *press;
-    const String text = U"Welcome";
+    String text = U"Welcome";
 
 public:
     Login(const InitData& init): IScene(init) { // コンストラクタ（必ず実装
@@ -35,7 +37,7 @@ public:
     }
 
     void update() override { // 更新関数
-        if(/*db有無確認*/1) changeScene(U"CreatePassword"); // パスワード作成シーンに遷移
+        if(!Database.is_registered()) changeScene(U"CreatePassword"); // パスワード作成シーンに遷移
 
         const bool mouseOver = button->mouseOver();
         if(mouseOver) Cursor::RequestStyle(CursorStyle::Hand); // 円の上にマウスカーソルがあれば
@@ -48,10 +50,21 @@ public:
         // center から (4, 4) ずらした位置を中心にテキストを描く
         (*font)(text).drawAt(center->movedBy(4, 4), ColorF(106, 90, 205, 0.5));
         (*font)(text).drawAt(*center);
-        SimpleGUI::TextBox(tes, Vec2(200, 420), 250, 18);
+        SimpleGUI::TextBox(tes, Vec2(200, 420), 250, 64);
         if(SimpleGUI::Button(U"Clear", Vec2(470, 420))) tes.clear();
-        if(button->leftPressed()){
-            if(/*ログイン関数*/1) changeScene(U"MainScene", 1.0s); // メインシーンに 1 秒かけて遷移
+        if(button->leftClicked()){
+            int cnt = 0;
+            bool valid = true;
+            for(char32_t var: tes.text) {
+                if(U'!'<=var && var<=U'~') cnt++;
+                else{
+                    valid = false;
+                    break;
+                }
+            }
+            if(!valid || !cnt) text = U"Invalid Password.";
+            else if(Database.login(tes.text)) changeScene(U"MainScene", 1.0s); // メインシーンに 1 秒かけて遷移
+            else text = U"Wrong Password";
         }
     }
 
@@ -67,7 +80,7 @@ private:
     Circle* button1;
     Transition* press;
     Font* font1;
-    const String text = U"Please create your password";
+    String text = U"Please create your password";
     TextEditState tes1;
 
 public:
@@ -91,7 +104,7 @@ public:
 
         // text の文字数以上の length は切り捨てられる
         (*font1)(text.substr(0, length)).drawAt(Scene::Center(), Color(41, 26, 33));
-        SimpleGUI::TextBox(tes1, Vec2(280, 340), 250, 18);
+        SimpleGUI::TextBox(tes1, Vec2(280, 340), 250, 64);
 
         const bool mouseOver = (*button1).mouseOver();
         if(mouseOver) Cursor::RequestStyle(CursorStyle::Hand); // マウスカーソルを手の形に
@@ -100,6 +113,23 @@ public:
         (*button1).movedBy(Vec2(0, 0).lerp(Vec2(0, 4), t))
             .drawShadow(Vec2(0, 6).lerp(Vec2(0, 1), t), 12 - t * 7, 5 - t * 4)
             .draw(*buttonColor1);
+
+        if(button1->leftClicked()){
+            int cnt = 0;
+            bool valid = true;
+            for(char32_t var: tes1.text) {
+                if(U'!'<=var && var<=U'~') cnt++;
+                else{
+                    valid = false;
+                    break;
+                }
+            }
+            if(valid && cnt){
+                Database.register_passwd(tes1.text);
+                changeScene(U"MainScene", 1.0s); // メインシーンに 1 秒かけて遷移
+            }
+            else text = U"Invalid Password.";
+        }
     }
 
     void draw() const override { // 描画関数 (const 修飾)
@@ -107,42 +137,92 @@ public:
     }
 };
 
-struct PassUnit {
-    String serviceName;
-    String userName;
-    String password;
-
-    PassUnit(String _serviceName, String _userName, String _password) {
-        serviceName = _serviceName;
-        userName = _userName;
-        password = _password;
-    }
-};
-//パスワードの三項目をセットにした配列
-Array<PassUnit> passArray;
-
 // メインシーン
 class MainScene : public App::Scene {
-public:
-    MainScene(const InitData& init) : IScene(init)
-    {
+private:
+    /// <summary>パスワードの欄の表示を切り替えるための変数</summary>
+    static inline bool isVisiblePass = false;
+    static inline int popupIndex = -1;
 
+    /// <summary>ポップアップの状態を表す</summary>
+    enum PopupState {
+        notPopup,
+        forAdd,
+        forEdit,
+        confirming,
+        forDelete,
+    };
+    PopupState popupState = notPopup;
+    PopupState lastPopupState;
+
+    // デザイン用定数
+    struct Design {
+        static inline Color inFrame = Palette::Whitesmoke;
+        static inline Color frame = Palette::Lightgrey;
+        static inline Color background = Palette::White;
+        static inline Color fontColor = Palette::Black;
+
+        static inline Color deletePopupBG = Palette::Red;
+        static inline Color deletePopupString = Palette::Yellow;
+    };
+
+    // スクロール用変数
+    struct {
+        int current = 0;
+        int max = 10;
+        double wheel = 0.0;
+    } scroll;
+
+    // テキストボックス用
+    TextEditState serviceNameText, userNameText, passwordText;
+
+    // 完了通知用
+    double noticeTimer = 5.0;
+    enum {
+        notice_copy,
+        notice_delete,
+        notice_add,
+        notice_edit,
+    } noticeType;
+
+    Vec2 screenSize;
+
+    Vec2 ratioPos(double x, double y) {
+        return Vec2(screenSize.x * x, screenSize.y * y);
+    }
+
+public:
+    MainScene(const InitData& init) : IScene(init) {
+        Scene::SetBackground(Design::background);
+        System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+        Window::SetTitle(U"Password Manager");
+        Window::SetStyle(WindowStyle::Sizable);
+        Scene::SetScaleMode(ScaleMode::ResizeFill);
+
+        //通常表示用フォントアセット
+        FontAsset::Register(U"Regular", 20);
+
+        //画像アセット
+        TextureAsset::Register(U"copy", U"images/copy.png");
+        TextureAsset::Register(U"edit", U"images/edit.png");
+        TextureAsset::Register(U"delete", U"images/delete.png");
+        TextureAsset::Register(U"visible", U"images/visible.png");
+        TextureAsset::Register(U"invisible", U"images/invisible.png");
+
+        passArray = Database.read_data();
     }
 
     // 更新関数
     void update() override {
-        Setup();
         ClearPrint();
         noticeTimer += Scene::DeltaTime();
-        screenSize = Vec2(Window::ClientWidth() , Window::ClientHeight());
-        scroll.max = floor((screenSize.y - 100) / 50);
+        screenSize = Vec2(Window::ClientWidth(), Window::ClientHeight());
+        scroll.max = floor((screenSize.y-100)/50);
 
-        //■
-
-        if (Rect(50, 50, screenSize.x - 100, screenSize.y - 110).drawFrame(10, Design::frame).draw(Design::inFrame).mouseOver() && popupState == notPopup) {
+        if (Rect(50, 50, screenSize.x-100, screenSize.y-110).drawFrame(10, Design::frame).draw(Design::inFrame).mouseOver() && popupState == notPopup) {
             scroll.wheel += Mouse::Wheel();
             if (scroll.wheel < 0) scroll.wheel = 0;
-            if (scroll.wheel > passArray.size() - 1) scroll.wheel = passArray.size() - 1;
+            if (scroll.wheel > passArray.size()-1) scroll.wheel = passArray.size()-1;
 
             scroll.current = floor(scroll.wheel);
         }
@@ -151,38 +231,28 @@ public:
         FontAsset(U"Regular")(U"ユーザー名").draw(ratioPos(0.35,0).x, 10, Design::fontColor);
         FontAsset(U"Regular")(U"パスワード").draw(ratioPos(0.6,0).x, 10, Design::fontColor);
 
-        if (TextureAsset(isVisiblePass ? U"visible" : U"invisible").resized(30).draw(Arg::center(ratioPos(0.6, 0).x + 130, isVisiblePass ? 25 : 26)).mouseOver() && popupState == notPopup) {
+        if (TextureAsset(isVisiblePass ? U"visible" : U"invisible").resized(30).draw(Arg::center(ratioPos(0.6, 0).x+130, isVisiblePass ? 25 : 26)).mouseOver() && popupState == notPopup) {
             Cursor::RequestStyle(CursorStyle::Hand);
-            if (MouseL.down()) {
-                isVisiblePass ^= true;
-                //■パスワードの可視/不可視処理
-
-            }
+            if (MouseL.down()) isVisiblePass ^= true;
         }
 
-        if (SimpleGUI::Button(U"追加", Vec2(screenSize.x - 130, 5))) {
-            popupState = forAdd;
-        }
-
+        if (SimpleGUI::Button(U"追加", Vec2(screenSize.x - 130, 5))) popupState = forAdd;
 
         for (int i = 0; i < scroll.max; i++) {
             if (scroll.current + i >= passArray.size()) break;
+            int height = 50 + 50*i;
 
-            int height = 50 + 50 * i;
+            // Array passArrayのインデックスは scroll.current + i
+            FontAsset(U"Regular")(passArray[scroll.current + i].service_name).draw(70, height, Design::fontColor);
+            FontAsset(U"Regular")(passArray[scroll.current + i].user_name).draw(ratioPos(0.35, 0).x, height, Design::fontColor);
+            FontAsset(U"Regular")(isVisiblePass ? passArray[scroll.current + i].password : U"*****").draw(ratioPos(0.6, 0).x, height, Design::fontColor);
 
-            FontAsset(U"Regular")(passArray[scroll.current + i].serviceName).draw(70, height, Design::fontColor);
-            FontAsset(U"Regular")(passArray[scroll.current + i].userName).draw(ratioPos(0.35, 0).x, height, Design::fontColor);
-            FontAsset(U"Regular")(passArray[scroll.current + i].password).draw(ratioPos(0.6, 0).x, height, Design::fontColor);
-
-
-            //Array passArrayのインデックスは scroll.current + i
-
-            if (TextureAsset(U"copy").resized(30).draw(screenSize.x - 160, height).mouseOver() && popupState == notPopup) {
+            if (TextureAsset(U"copy").resized(30).draw(screenSize.x-160, height).mouseOver() && popupState == notPopup) {
                 Cursor::RequestStyle(CursorStyle::Hand);
                 if (MouseL.down()) {
-                    //■パスワードのコピー処理
-
-                    //コピーできたら以下をする
+                    // パスワードのコピー処理
+                    Clipboard::SetText(passArray[scroll.current + i].password);
+                    // コピーできたら以下をする
                     noticeType = notice_copy;
                     noticeTimer = 0.0;
                 }
@@ -201,10 +271,11 @@ public:
                     popupState = forDelete;
                 }
             }
-
         }
 
         RectF(Vec2(0,0),screenSize).draw(ColorF(Palette::Black, popupState == notPopup ? 0.0 : 0.5));
+        int cnt = 0;
+        bool valid = true;
         //ポップアップ時
         switch (popupState) {
             case forAdd:
@@ -212,169 +283,106 @@ public:
             case confirming:
                 RectF(ratioPos(0.15,0.25), ratioPos(0.7,0.5)).draw(Design::background);
 
+                if(popupState == forEdit){
+                    serviceNameText.text = passArray[popupIndex].service_name;
+                    userNameText.text = passArray[popupIndex].user_name;
+                    passwordText.text = passArray[popupIndex].password;
+                }
                 FontAsset(U"Regular")(U"サービス名").draw(ratioPos(0.2,0.3), Design::fontColor);
                 SimpleGUI::TextBox(serviceNameText, ratioPos(0.2,0.35), screenSize.x / 4, unspecified, popupState != confirming);
                 FontAsset(U"Regular")(U"ユーザー名").draw(ratioPos(0.2, 0.42), Design::fontColor);
                 SimpleGUI::TextBox(userNameText, ratioPos(0.2, 0.47), screenSize.x / 4, unspecified, popupState != confirming);
                 FontAsset(U"Regular")(U"パスワード").draw(ratioPos(0.2, 0.54), Design::fontColor);
                 SimpleGUI::TextBox(passwordText, ratioPos(0.2, 0.59), screenSize.x /4, unspecified, popupState != confirming);
+
+                for(char32_t var: passwordText.text) {
+                    if(U'!'<=var && var<=U'~') cnt++;
+                    else{
+                        valid = false;
+                        break;
+                    }
+                }
                 
-                
-                if (popupState != confirming) {
+                if (popupState != confirming || !valid || !cnt) {
                     FontAsset(U"Regular")(popupState == forAdd ? U"パスワードの追加" : U"パスワードの変更").draw(ratioPos(0.5,0.3), Design::fontColor);
+                    if(!valid || !cnt) FontAsset(U"Regular")(U"0文字以上の ASCII 文字のみ\n対応です。").draw(ratioPos(0.5, 0.4), Design::fontColor);
                     if (SimpleGUI::Button(U"決定", ratioPos(0.5,0.5))) {
                         lastPopupState = popupState;
                         popupState = confirming;
                     }
-                    if (SimpleGUI::Button(U"キャンセル", ratioPos(0.5,0.65))) {
-                        popupState = notPopup;
-                    }
-                    
+                    if (SimpleGUI::Button(U"キャンセル", ratioPos(0.5,0.65))) popupState = notPopup;
                 }
                 else {
-                    //再確認
+                    // 再確認
                     RectF(ratioPos(0.5,0.3),ratioPos(0.33,0.4)).drawFrame(5, Design::frame);
                     FontAsset(U"Regular")(U"この内容で\n確定しますか？").draw(ratioPos(0.57,0.38), Design::fontColor);
                     if (SimpleGUI::Button(U"はい", ratioPos(0.54, 0.58),80)) {
-                        //■パスワードの追加・変更処理
-                        //追加：lastPopupState == forAdd のとき
-                        //変更：lastPopupState == forEdit のとき
-                        //      Array passArrayのインデックスは popupIndex
-
+                        single_data temp(serviceNameText.text, userNameText.text, passwordText.text);
+                        // パスワードの追加・変更処理
+                        // Array passArray のインデックスは popupIndex
+                        if(lastPopupState == forAdd){ // 追加
+                            passArray << temp;
+                            noticeType = notice_add;
+                        }else if(lastPopupState == forEdit){ // 変更
+                            passArray[popupIndex] = temp;
+                            noticeType = notice_edit;
+                        }
+                        Database.write_data(passArray);
                         popupState = notPopup;
-
-                        //追加・変更できたら以下をする
-                        if(lastPopupState == forAdd) noticeType = notice_add;
-                        if(lastPopupState == forEdit) noticeType = notice_edit;
                         noticeTimer = 0.0;
                     }
-                    if (SimpleGUI::Button(U"いいえ", ratioPos(0.7,0.58),80)) {
-                        popupState = lastPopupState;
-                    }
+                    if (SimpleGUI::Button(U"いいえ", ratioPos(0.7,0.58),80)) popupState = lastPopupState;
                 }
                 break;
 
             case forDelete:
-                RectF(Arg::center(screenSize / 2), 300, 200).draw(Design::deletePopupBG);
-                FontAsset(U"Regular")(U"本当に削除しますか？").draw(Arg::center(screenSize.x * 0.5 , screenSize.y * 0.45), Design::deletePopupString);
+                RectF(Arg::center(screenSize/2), 300, 200).draw(Design::deletePopupBG);
+                FontAsset(U"Regular")(U"本当に削除しますか？").draw(Arg::center(screenSize.x*0.5 , screenSize.y*0.45), Design::deletePopupString);
 
                 if (SimpleGUI::Button(U"はい", ratioPos(0.5, 0.55) - Vec2(100,0), 80)) {
-                    //■パスワードの削除処理
-                    //Array passArrayのインデックスは popupIndex
-
-
+                    // パスワードの削除処理
+                    // Array passArrayのインデックスは popupIndex
+                    for(int j=popupIndex; j<passArray.size()-1; j++){
+                        passArray[j] = passArray[j+1];
+                    }
+                    passArray.pop_back();
+                    Database.write_data(passArray);
                     popupState = notPopup;
-                    //削除できたら以下をする
+
+                    // 削除できたら以下をする
                     noticeType = notice_delete;
                     noticeTimer = 0.0;
                 }
-                if (SimpleGUI::Button(U"いいえ", ratioPos(0.5,0.55) + Vec2(20,00), 80)) {
-                    popupState = notPopup;
-                }
+                if (SimpleGUI::Button(U"いいえ", ratioPos(0.5,0.55) + Vec2(20,00), 80)) popupState = notPopup;
                 break;
 
             default: break;
         }
         
-        int copyNoticeX = (2.5 - abs(noticeTimer - 2.5)) * 300;
+        int copyNoticeX = (2.5-abs(noticeTimer-2.5))*300;
         if (copyNoticeX > 200) copyNoticeX = 200;
-        Rect(screenSize.x - copyNoticeX, screenSize.y -50, 200, 50).draw(Design::fontColor);
+        Rect(screenSize.x-copyNoticeX, screenSize.y-50, 200, 50).draw(Design::fontColor);
         String noticeMessage;
         switch (noticeType) {
-            case notice_copy:  noticeMessage = U"コピーしました。"; break;
-            case notice_delete:noticeMessage = U"削除しました。"; break;
-            case notice_add:  noticeMessage = U"追加しました。"; break;
-            case notice_edit:noticeMessage = U"変更しました。"; break;
+            case notice_copy:
+                noticeMessage = U"コピーしました。";
+                break;
+            case notice_delete:
+                noticeMessage = U"削除しました。";
+                break;
+            case notice_add:
+                noticeMessage = U"追加しました。";
+                break;
+            case notice_edit:
+                noticeMessage = U"変更しました。";
+                break;
         }
         FontAsset(U"Regular")(noticeMessage).draw(screenSize.x + 20 - copyNoticeX, screenSize.y - 40, Design::background);
     }
 
-    /// <summary>パスワードの欄の表示を切り替えるための変数</summary>
-    static inline bool isVisiblePass = false;
-
-    /// <summary>ポップアップの状態を表す</summary>
-    enum PopupState {
-        notPopup,
-        forAdd,
-        forEdit,
-        confirming,
-        forDelete,
-    };
-    PopupState popupState = notPopup;
-    PopupState lastPopupState;
-
-    static inline int popupIndex = -1;
-
     void draw() const override {
 
     }
-
-    static inline bool isDoneSetup = false;
-    /// <summary>最初に一度だけ実行する</summary>
-    void Setup() {
-        if (isDoneSetup) return;
-        isDoneSetup = true;
-
-        Graphics::SetTargetFrameRateHz(30);
-        Scene::SetBackground(Design::background);
-        System::SetTerminationTriggers(UserAction::CloseButtonClicked);
-        Window::SetTitle(U"Password Manager");
-        Window::SetStyle(WindowStyle::Sizable);
-        Scene::SetScaleMode(ScaleMode::ResizeFill);
-
-        //通常表示用フォントアセット
-        FontAsset::Register(U"Regular", 20);
-
-        //画像アセット
-        TextureAsset::Register(U"copy", U"images/copy.png");
-        TextureAsset::Register(U"edit", U"images/edit.png");
-        TextureAsset::Register(U"delete", U"images/delete.png");
-        TextureAsset::Register(U"visible", U"images/visible.png");
-        TextureAsset::Register(U"invisible", U"images/invisible.png");
-
-        for (int i = 0; i < 20; i++) {
-            passArray << PassUnit(U"serviceName{}"_fmt(i), U"userName{}"_fmt(i), U"password{}"_fmt(i));
-        }
-    }
-
-
-    //デザイン用定数
-    struct Design {
-        static inline Color inFrame = Palette::Whitesmoke;
-        static inline Color frame = Palette::Lightgrey;
-        static inline Color background = Palette::White;
-        static inline Color fontColor = Palette::Black;
-
-        static inline Color deletePopupBG = Palette::Red;
-        static inline Color deletePopupString = Palette::Yellow;
-    };
-    Vec2 screenSize;
-    
-    Vec2 ratioPos(double x, double y) {
-        return Vec2(screenSize.x * x, screenSize.y * y);
-    }
-
-    //スクロール用変数
-    struct {
-        int current = 0;
-        int max = 10;
-
-        double wheel = 0.0;
-    }scroll;
-
-    //テキストボックス用
-    TextEditState serviceNameText;
-    TextEditState userNameText;
-    TextEditState passwordText;
-
-    //完了通知用
-    double noticeTimer = 5.0;
-    enum {
-        notice_copy,
-        notice_delete,
-        notice_add,
-        notice_edit,
-    }noticeType;
 };
 
 void kowerkoint_dbg() {
@@ -407,6 +415,7 @@ void Main()
     manager.add<MainScene>(U"MainScene"); // メインシーン（名前は U"MainScene"）を登録
     manager.setFadeColor(Palette::Skyblue); // フェードイン・フェードアウト時の画面の色
     Window::SetStyle(WindowStyle::Sizable);
+    Graphics::SetTargetFrameRateHz(60);
 
     //kowerkoint_dbg();
     while(System::Update()){
--- crypto.hpp
+++ crypto.hpp
@@ -28,21 +28,16 @@ String binary_to_hexstring(const unsigned char* binary, int size) {
 String sha256(String text) {
     unsigned char digest[SHA256_DIGEST_LENGTH];
     string str = Unicode::NarrowAscii(text);
-    const char* cc = str.c_str();
-    const unsigned char* cuc = reinterpret_cast<const unsigned char*>(cc);
-    SHA256(cuc, sizeof(cuc) - 1, digest);
+    const unsigned char* cuc = (const unsigned char*)str.c_str();
+    SHA256(cuc, str.length(), digest);
     return binary_to_hexstring(digest, SHA256_DIGEST_LENGTH);
 }
 
 String aes256_encrypt(String text, String key) {
     string text_string = Unicode::NarrowAscii(text);
-    const char* text_cc = text_string.c_str();
-    const unsigned char* text_cuc = reinterpret_cast<const unsigned char*>(text_cc);
-
-
     string key_string = Unicode::NarrowAscii(key);
-    const char* key_cc = key_string.c_str();
-    const unsigned char* key_cuc = reinterpret_cast<const unsigned char*>(key_cc);
+    const unsigned char* key_cuc = (const unsigned char*)text_string.c_str();
+    const unsigned char* text_cuc = (const unsigned char*)key_string.c_str();
 
     unsigned char enc_out[256];
 
@@ -50,24 +45,19 @@ String aes256_encrypt(String text, String key) {
     AES_set_encrypt_key(key_cuc, 256, &enc_key);
     AES_encrypt(text_cuc, enc_out, &enc_key);
 
-    //Print << U"original:" << binary_to_hexstring(text_cuc, 256);
-    //Print << U"crypted:" << binary_to_hexstring(enc_out, 256);
-
     return binary_to_hexstring(enc_out, 256);
 }
 
 String aes256_decrypt(String hex_text, String key) {
     unsigned char binary_text[256];
     string hex_text_string = Unicode::NarrowAscii(hex_text); //一度置かないとダングリングする
-    const char* cstr_text = hex_text_string.c_str();
     int size = hex_text_string.length() / 2;
     for (int i = 0; i < size; i++) {
         binary_text[i] = (unsigned char)stol(hex_text_string.substr(i * 2, 2), nullptr, 16);
     }
 
     string key_string = Unicode::NarrowAscii(key); //一度置かないとダングリングする
-    const char* key_cc = key_string.c_str();
-    const unsigned char* key_cuc = reinterpret_cast<const unsigned char*>(key_cc);
+    const unsigned char* key_cuc = (const unsigned char*)key_string.c_str();
 
     unsigned char dec_out[256];
 
@@ -75,7 +65,5 @@ String aes256_decrypt(String hex_text, String key) {
     AES_set_decrypt_key(key_cuc, 256, &dec_key);
     AES_decrypt(binary_text, dec_out, &dec_key);
 
-    //Print << U"original:" << binary_to_hexstring(binary_text, 256);
-    //Print << U"decrypted:" << binary_to_hexstring(dec_out, 256);
     return Unicode::WidenAscii(std::string(reinterpret_cast<const char*>(dec_out)));
 }
